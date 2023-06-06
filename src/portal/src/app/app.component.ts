e2f7928be9901cd0c5ebd961fffc5a85b6e2ed2a
Carlos Cabrera <c.cabrera@ciencias.unam.mx>
2023-06-06 02:22:32 +0200

    ref(PAEM): Se eliminan imports sin usar

--- src/portal/src/app/app.component.ts
+++ src/portal/src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component } from '@angular/core';
-import {AuthService} from "./auth/auth.service";
-import {Subscription} from "rxjs";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
--- src/portal/src/app/auth/auth.module.ts
+++ src/portal/src/app/auth/auth.module.ts
@@ -1,15 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {TokenInterceptor} from "./interceptors/token.interceptor";
-import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
 import {MatIconModule} from "@angular/material/icon";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
-import {MatError, MatFormFieldModule} from "@angular/material/form-field";
+import {MatFormFieldModule} from "@angular/material/form-field";
 import {LoginComponent} from "./login/login.component";
-import {AuthService} from "./auth.service";
 
 
 @NgModule({
